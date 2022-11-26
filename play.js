64539f65463998c64c5c5ad3dcb5d15a94311a22
Awo Moussa <amoussab@ualberta.ca>
2022-11-26 05:29:28 +0000

    snake game project

--- play.js
+++ play.js
@@ -1,10 +1,3 @@
-// const net = require("net");
-// const connect = require('./client');
-
-
-
-// console.log("Connecting ...");
-// connect();
 
 const connect  = require("./client");
 const setupInput = require("./input");
